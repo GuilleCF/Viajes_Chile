5c1a9e769f6014fa1ec33d9734eba89d49870609
Guillermo Carrasco Faus <72774022+SrWilly@users.noreply.github.com>
2024-02-17 09:47:32 -0300

    add documentation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-// Activate the tooltip
+// Init the tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
@@ -9,7 +9,7 @@ $(document).ready(function(){
             event.preventDefault(); 
             var hash = this.hash;
             $('html, body').animate({
-                scrollTop: $(hash).offset().top - 100
+                scrollTop: $(hash).offset().top
             }, 800, function(){
             window.location.hash = hash;
             }); 
